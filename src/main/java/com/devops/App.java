556acfefd229349d0588bfeee364e4aab8108da2
root <root@ip-172-31-43-104.ap-south-1.compute.internal>
2025-03-16 09:04:43 +0000

    Java Maven Selenium Project Added

---
+++ src/main/java/com/devops/App.java
@@ -0,0 +1,8 @@
+package com.devops;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("🚀 DevOps CI/CD Pipeline Successfully Executed!");
+    }
+}
+
---
+++ src/test/java/com/devops/LoginTest.java
@@ -0,0 +1,12 @@
+package com.devops;
+
+import org.junit.Test;
+
+public class LoginTest {
+
+    @Test
+    public void testLogin() {
+        System.out.println(" Selenium Test Executed Successfully!");
+    }
+}
+
